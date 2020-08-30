f1eba35e4c4fddb431a2390955a5242c2292440b
emmanuel <ejariza@gmail.com>
2020-08-29 23:29:32 -0500

    Added Markov generator

--- src/aisong/utils.clj
+++ src/aisong/utils.clj
@@ -3,6 +3,11 @@
              [leipzig.melody :refer :all]
              [overtone.music.pitch :as pitch]))
 
+
+(defn remap [m f]
+  "remaps all elements fron the hash-map m applying them all the function f"
+  (into {} (for [[k v] m] [k (f v)])))
+
 (defn apply-drum
   "Applies drums phrase until the end of melody phrase"
   [drums mel]
@@ -115,3 +120,35 @@
 
 (defn play-chord [a-chord inst]
   (doseq [note a-chord] (inst note)))
+
+
+;;;;;;;;;;;;;;;;; Markov things ;;;;;;;;;;;;
+
+(defn next-markov-state
+  [current-state states probabilities]
+  (let [conditional-p       (nth probabilities
+                                 (.indexOf states current-state))
+        total               (reduce + conditional-p)
+        r                   (rand total)]
+    (loop [i 0 sum 0]
+      (if (< r (+ (conditional-p i) sum))
+        (nth states i)
+        (recur (inc i) (+ (conditional-p i) sum))))))
+
+
+(next-markov-state :b [:a   :b   :c  :d]
+                   [[0.8 0.2 0.7 0.3]
+                    [0.3 0.4 0.5 0.1]
+                    [0.1 0.4 0.9 0.3]
+                    [0.1 0.4 0.1 0.7]])
+
+(defn generate-markov-chain
+  [initial-state states probabilities length]
+  (loop [i length chain [initial-state]]
+    (let [next-state (next-markov-state (last chain)
+                                        states
+                                        probabilities)
+          updated-chain (conj chain next-state)]
+      (if (= i 0)
+        updated-chain
+        (recur (- i 1) updated-chain)))))
